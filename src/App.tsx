5428ef769dd146af294a06a51dee8e5775af267c
Sean Truong <seantruong@Seans-MacBook-Pro.local>
2023-09-23 14:44:45 +0700

    init tailwind vite

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='flex flex-col gap-6'>
+      <div className=' text-center'>
+        <h1 className=' text-2xl font-extrabold mb-3'>Join our Community of Developers</h1>
+        <div>To join our community and participate in frontend challenges.</div>
+        <div>Please fill out the following form.</div>
+      </div>
+      <div className=' w-full max-w-3xl bg-white drop-shadow-lg rounded-md'>
+        <h1>
+          Form component
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
