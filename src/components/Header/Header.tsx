ef32faf4b7bcbacbc7ea56e400d749c65f4e4a96
Sean Truong <seantruong@Seans-MacBook-Pro.local>
2023-09-24 14:56:35 +0700

    restructure

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import { Fragment } from "react";
+
+export function Header() {
+  return (
+    <Fragment>
+      <div className="text-center">
+        <h1 className="text-2xl font-extrabold mb-3">
+          Join our Community of Developers
+        </h1>
+        <h3>To join our community and participate in frontend challenges.</h3>
+        <h3>Please fill out the following form.</h3>
+      </div>
+    </Fragment>
+  );
+}
