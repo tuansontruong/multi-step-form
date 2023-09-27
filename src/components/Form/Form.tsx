766c7aea7c0acb72752911c34a5a27ff0d5b8237
Sean Truong <seantruong@Seans-MacBook-Pro.local>
2023-09-27 22:30:07 +0700

    complete personal information ui

--- src/components/Form/Form.tsx
+++ src/components/Form/Form.tsx
@@ -1,3 +1,5 @@
+import { CTAs } from "../CTAs";
+import { PersonalInformation } from "../PersonalInformation";
 import { Seperator } from "../Seperator";
 import { Steps } from "../Steps";
 
@@ -5,8 +7,11 @@ export function Form() {
   return (
     <div className="w-full max-w-3xl bg-white drop-shadow-lg rounded-md p-4">
       <div className="flex flex-col gap-6">
-        <Steps numberOfSteps={4} currentStep={2} />
+        <Steps numberOfSteps={4} currentStep={1} />
         <Seperator />
+        <PersonalInformation />
+        <Seperator />
+        <CTAs />
       </div>
     </div>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,11 +4,13 @@ export function Header() {
   return (
     <Fragment>
       <div className="text-center">
-        <h1 className="text-2xl font-extrabold mb-3">
+        <h1 className="text-3xl font-black mb-3">
           Join our Community of Developers
         </h1>
-        <h3>To join our community and participate in frontend challenges.</h3>
-        <h3>Please fill out the following form.</h3>
+        <h3 className=" text-gray">
+          To join our community and participate in frontend challenges.
+        </h3>
+        <h3 className="text-gray">Please fill out the following form.</h3>
       </div>
     </Fragment>
   );
