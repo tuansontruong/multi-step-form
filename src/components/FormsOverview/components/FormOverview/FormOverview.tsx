9ad39155c3c0fa7e1c23106b73967b9f1a759fb2
Sean Truong <seantruong@Seans-MacBook-Pro.local>
2023-09-28 10:24:41 +0700

    update

--- src/components/FormsOverview/components/FormOverview/FormOverview.tsx
+++ src/components/FormsOverview/components/FormOverview/FormOverview.tsx
@@ -1,18 +1,16 @@
 import { Separator } from "@common";
 
 import { FormSteps } from "../FormSteps";
+import { PersonalInformationForm } from "../PersonalInformationForm";
 import { FormFooter } from "../FormFooter";
-
-import { PersonalInformation } from "./components";
-
 
 export function FormOverview() {
   return (
     <div className="w-full max-w-3xl bg-white drop-shadow-lg rounded-md p-4">
       <div className="flex flex-col gap-6">
-        <FormSteps numberOfSteps={4} currentStep={1} />
+        <FormSteps numberOfSteps={4} currentStep={2} />
         <Separator />
-        <PersonalInformation />
+        <PersonalInformationForm />
         <Separator />
         <FormFooter />
       </div>
--- src/components/FormsOverview/components/PersonalInformationForm/PersonalInformationForm.tsx
+++ src/components/FormsOverview/components/PersonalInformationForm/PersonalInformationForm.tsx
@@ -1,6 +1,6 @@
 export interface IPersonalInformationProps {}
 
-export function PersonalInformation() {
+export function PersonalInformationForm() {
   return (
     <div>
       <h3 className="text-2xl font-extrabold">Personal Information</h3>
