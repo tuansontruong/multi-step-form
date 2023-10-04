c8ea5bd974bd2f0e8a55949a76583922c4fcc746
Sean Truong <seantruong@Seans-MacBook-Pro.local>
2023-10-04 16:41:11 +0700

    add hooks

---
+++ src/components/FormsOverview/hooks/useLocationHash.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useState } from "react";
+import { ROUTES, ROUTES_KEY } from "../types/routes";
+
+export const useLocationHash = () => {
+  const [currentHash, setCurrentHash] = useState<ROUTES_KEY>(
+    ROUTES.PersonalInformation
+  );
+  const onHashChange = () => {
+    if (window.location.hash === "") {
+      window.location.hash = ROUTES.PersonalInformation;
+    }
+    setCurrentHash(window.location.hash as ROUTES_KEY);
+  };
+  const orderedHashes = Object.values(ROUTES);
+
+  const proceedToNextHash = () => {
+    const currentStepIndex = orderedHashes.findIndex((step) => step === currentHash);
+    const nextStepIndex = currentStepIndex + 1;
+    const nextStep = orderedHashes[nextStepIndex];
+    if (nextStep) {
+      window.location.hash = nextStep;
+    }
+  };
+  const goBackToPrevHash = () => {
+    const currentStepIndex = orderedHashes.findIndex((step) => step === currentHash);
+    const nextStepIndex = currentStepIndex - 1;
+    const nextStep = orderedHashes[nextStepIndex];
+    if (nextStep) {
+      window.location.hash = nextStep;
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("hashchange", onHashChange);
+    window.addEventListener("load", onHashChange);
+    return () => {
+      window.removeEventListener("hashchange", onHashChange);
+      window.addEventListener("load", onHashChange);
+    };
+  }, []);
+  return { currentHash, proceedToNextHash, goBackToPrevHash };
+};
